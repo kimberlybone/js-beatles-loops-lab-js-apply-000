3f95b22e5babb4feada9b86a064ae068bdfc2f6b
Kimberly Bone <k_bone@uncg.edu>
2019-05-14 01:06:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments){
+  var newMusicians = []
+  for (let allMusicians = 0; allMusicians < 10; allMusicians++) {
+    console.log(`${musicians} + 'plays' + ${instruments}`);
+  }
+  console.log(newMusicians.push(allMusicians));
+  return newMusicians;
+}
+
+function johnLennonFacts(facts){
+  const facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
+let i = 0
+while (i < facts.length) {
+  console.log(facts[i] + '!!!');
+  i++
+  return johnLennonFacts;
+}
+}
+
+function iLoveTheBeatles(number) {
+  let statement =[]
+}
