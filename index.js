ee3a0fc485a0c49577dd4f79d9ffedf0498a8884
Kimberly Bone <k_bone@uncg.edu>
2019-05-14 01:10:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,15 +8,15 @@ function theBeatlesPlay(musicians, instruments){
 }
 
 function johnLennonFacts(facts){
-  const facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian",
+  const newFacts = ["He was the last Beatle to learn to drive", "He was never a vegetarian",
   "He was a choir boy and boy scout",
   "He hated the sound of his own voice"
 ];
 let i = 0
-while (i < facts.length) {
-  console.log(facts[i] + '!!!');
+while (i < newFacts.length) {
+  console.log(newFacts[i] + '!!!');
   i++
-  return johnLennonFacts;
+  return newFacts;
 }
 }
 
