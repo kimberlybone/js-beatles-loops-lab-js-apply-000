d321bf58e0be52bea88f0b8e4d1e71873815d3f3
Kimberly Bone <k_bone@uncg.edu>
2019-05-14 01:16:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function theBeatlesPlay(musicians, instruments){
   var newMusicians = [];
   for (let i = 0; i < musicians.length; i++) {
-    newMusicians.push(`${musicians} + 'plays' + ${instruments}`);
+    newMusicians.push(musicians[i] + 'plays' + instruments[i]);
   }
   return newMusicians;
 }
